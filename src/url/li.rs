57b4c06514a955d72e078145d65c3b800c6042da
wacdev <i@wac.tax>
2023-06-18 08:04:40 +0800

    ♨

--- src/url/li.rs
+++ src/url/li.rs
@@ -10,18 +10,20 @@ url_fn!(post() {
     let li = img_li().await?;
     let mut vec = VecAny::new();
     for i in li {
+        let mut t = VecAny::new();
         let id:i64 = i.get(0);
-        vec.push(id as u64);
+        t.push(id as u64);
         let hash:Vec<u8> = i.get(1);
-        vec.push(hash);
+        t.push(hash);
         let w:u32 = i.get(2);
-        vec.push(w);
+        t.push(w);
         let h:u32 = i.get(3);
-        vec.push(h);
+        t.push(h);
         let star:i64 = i.get(4);
-        vec.push(star as u64);
+        t.push(star as u64);
         let reply:i64 = i.get(5);
-        vec.push(reply as u64);
+        t.push(reply as u64);
+        vec.push(t);
     }
     vec
 });
