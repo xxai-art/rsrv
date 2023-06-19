e67cbb166f4588d21121899fb5a6cd15a821a791
xxai.art <xxai.art@gmail.com>
2023-06-19 15:03:13 +0800

    ♨

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ exe=./target/debug/$name
 rm $exe
 
 # 不用 cargo run，因为 watchexe 和 cargo run 一起用总是会端口冲突，不知道为什么
-cargo build -p $name
+yes | cargo build -p $name
 
 GREEN='\033[0;92m'
 NC='\033[0m'
@@ -19,5 +19,8 @@ NC='\033[0m'
 echo -e "\n${GREEN}❯ $exe$NC\n"
 pkill -9 $name || true
 if [ -f "$exe" ]; then
-  $exe && exit 0 || $exe
+  ($exe && exit 0) || (
+    pkill -9 $name || true
+    $exe
+  )
 fi
--- xxai/src/lib.rs
+++ xxai/src/lib.rs
@@ -18,3 +18,19 @@ pub fn is_ascii_digit(bytes: &[u8]) -> bool {
     i.is_ascii_digit()
   })
 }
+
+pub fn zip_u64(li: impl IntoIterator<Item = u64>) -> Result<Vec<u8>> {
+  let mut u64_li = vec![];
+  for i in li {
+    let i: i64 = i.try_into()?;
+    u64_li.push(i as u64);
+  }
+  Ok(vbyte::compress_list(&u64_li))
+}
+
+pub fn unzip_u64(bin: impl AsRef<[u8]>) -> Vec<u64> {
+  match vbyte::decompress_list(bin.as_ref()) {
+    Ok(r) => r,
+    Err(_) => vec![],
+  }
+}
