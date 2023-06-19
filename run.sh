c029e832a3c5a0764c10840082559a3d8de7ecc1
xxai.art <xxai.art@gmail.com>
2023-06-19 12:14:13 +0800

    ♨

--- run.sh
+++ run.sh
@@ -8,13 +8,17 @@ name=${name//\'/}
 
 mkdir -p out
 
+exe=./target/debug/$name
+rm $exe
+
 # 不用 cargo run，因为 watchexe 和 cargo run 一起用总是会端口冲突，不知道为什么
 cargo build
 
 GREEN='\033[0;92m'
 NC='\033[0m'
 
-exe=./target/debug/$name
 echo -e "\n${GREEN}❯ $exe$NC\n"
 pkill -9 $name || true
-exec $exe
+if [ -f "$exe" ]; then
+  exec $exe
+fi
--- src/url/star.rs
+++ src/url/star.rs
@@ -1,5 +1,9 @@
 use anypack::sync_url_fn;
 use tower_cookies::Cookies;
+use xkv::fred::interfaces::HashesInterface;
+
+use crate::R;
+
 // use anypack::url_fn;
 // use xxpg::Q;
 //
@@ -8,10 +12,18 @@ use tower_cookies::Cookies;
 //     SELECT task.id,hash::bytea,w,h,star,laugh FROM bot.task,bot.civitai_img WHERE hash IS NOT NULL AND bot.task.rid=bot.civitai_img.id AND task.adult=0 AND cid=1 ORDER BY star DESC LIMIT 600
 // );
 
+const TOKEN_LEN: usize = 8;
+
+static mut SK: [u8; 32] = [0; 32];
+
 sync_url_fn!(post(cookies: Cookies) {
+    let sk:Vec<u8> = R.get().unwrap().hget("conf","SK").await?;
+    dbg!(sk.len() );
     if let Some(c) = cookies.get("I") {
-        let c = xxai::cookie_decode(c.value());
-        dbg!(c);
+        let c = xxai::cookie_decode(&c.value())?;
+        let token = &c[..TOKEN_LEN];
+        let client = &c[TOKEN_LEN..];
+        dbg!(token, client);
     }
     1
 });
