fcff1106ba7c205cc6310c3d8101442558a02a2c
xxai.art <xxai.art@gmail.com>
2023-06-19 11:57:27 +0800

    ♨

--- run.sh
+++ run.sh
@@ -2,13 +2,20 @@
 
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
-set -ex
 
 name=$(dasel package.name -f Cargo.toml)
 name=${name//\'/}
 
 mkdir -p out
-# 不直接用cargo run，是因为watchexe和cargo run一起用总是会端口冲突，不知道为什么
-cargo build -Z unstable-options
+
+# 不用 cargo run，因为 watchexe 和 cargo run 一起用总是会端口冲突，不知道为什么
+cargo build
+
 pkill -9 $name || true
-./target/debug/$name
+
+GREEN='\033[0;92m'
+NC='\033[0m'
+
+exe=./target/debug/$name
+echo -e "\n${GREEN}❯ $exe$NC\n"
+exec $exe
