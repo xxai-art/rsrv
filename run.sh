ca41a7cd1f76499a4c1950cae26d29dd1fa75d49
xxai.art <xxai.art@gmail.com>
2023-06-19 11:58:39 +0800

    ♨

--- run.sh
+++ run.sh
@@ -11,11 +11,10 @@ mkdir -p out
 # 不用 cargo run，因为 watchexe 和 cargo run 一起用总是会端口冲突，不知道为什么
 cargo build
 
-pkill -9 $name || true
-
 GREEN='\033[0;92m'
 NC='\033[0m'
 
 exe=./target/debug/$name
 echo -e "\n${GREEN}❯ $exe$NC\n"
+pkill -9 $name || true
 exec $exe
