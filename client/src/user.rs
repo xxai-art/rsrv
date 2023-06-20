8fcb1f4400b1e21cd0e6a5db90823ae57177edc1
xxai.art <xxai.art@gmail.com>
2023-06-20 13:56:01 +0800

    ♨

--- client/src/user.rs
+++ client/src/user.rs
@@ -5,16 +5,17 @@ const R_CLIENT_USER: &[u8] = &[4, 0];
 const ZMAX: &str = "zmax";
 
 impl crate::Client {
-  pub async fn user_id(&mut self) -> Option<u64> {
-    if let Some(id) = self._user_id {
+  pub async fn user_id(&mut self) -> anyhow::Result<Option<u64>> {
+    Ok(if let Some(id) = self._user_id {
       if id == 0 {
         let key = [R_CLIENT_USER, &xxai::u64_bin(self.id)].concat();
-        let id: Option<Vec<u8>> = R.fcall_ro(ZMAX, vec![&key[..]], vec![0]).await.unwrap();
-        let id = if let Some(id) = id {
-          Some(bin_u64(id))
-        } else {
-          None
-        };
+        let id: Option<u64> = R.fcall_ro(ZMAX, vec![&key[..]], vec![0]).await?;
+        // let id: Option<Vec<u8>> = R.fcall_ro(ZMAX, vec![&key[..]], vec![0]).await.unwrap();
+        // let id = if let Some(id) = id {
+        //   Some(bin_u64(id))
+        // } else {
+        //   None
+        // };
         self._user_id = id;
         id
       } else {
@@ -22,6 +23,6 @@ impl crate::Client {
       }
     } else {
       None
-    }
+    })
   }
 }
