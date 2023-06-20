332b266898cafa36dc5df733c3a5048020c2f451
xxai.art <xxai.art@gmail.com>
2023-06-20 12:52:34 +0800

    ♨

--- rsrv/src/url/star.rs
+++ rsrv/src/url/star.rs
@@ -13,8 +13,9 @@ use axum::Extension;
 //     SELECT task.id,hash::bytea,w,h,star,laugh FROM bot.task,bot.civitai_img WHERE hash IS NOT NULL AND bot.task.rid=bot.civitai_img.id AND task.adult=0 AND cid=1 ORDER BY star DESC LIMIT 600
 // );
 
-sync_url_fn!(post(Extension(client):Extension<client::Client>) {
+sync_url_fn!(post(Extension(mut client):Extension<client::Client>) {
     // client(host, &cookies);
+    dbg!(client.user_id().await);
     dbg!(client);
     1
 });
