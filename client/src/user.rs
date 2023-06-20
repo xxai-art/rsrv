1c132f475b31f35c716103c272bd9443239b0025
xxai.art <xxai.art@gmail.com>
2023-06-20 12:52:45 +0800

    ♨

--- client/src/user.rs
+++ client/src/user.rs
@@ -7,7 +7,7 @@ impl crate::Client {
   pub async fn user_id(&mut self) -> Option<u64> {
     if let Some(id) = self._user_id {
       if id == 0 {
-        let key = [R_CLIENT_USER, &xxai::u64_bin(id)].concat();
+        let key = [R_CLIENT_USER, &xxai::u64_bin(self.id)].concat();
         let id: Option<u64> = R.fcall_ro(ZMAX, key, 0).await.unwrap();
         dbg!(id);
         self._user_id = id;
