8cf06d8b76c3d205c3c09b67f571ac5245b2ac65
xxai.art <xxai.art@gmail.com>
2023-06-20 13:36:10 +0800

    ♨

--- client/src/user.rs
+++ client/src/user.rs
@@ -1,4 +1,5 @@
 use x0::{fred::interfaces::FunctionInterface, R};
+use xxai::bin_u64;
 
 const R_CLIENT_USER: &[u8] = &[4, 0];
 const ZMAX: &str = "zmax";
@@ -8,15 +9,18 @@ impl crate::Client {
     if let Some(id) = self._user_id {
       if id == 0 {
         let key = [R_CLIENT_USER, &xxai::u64_bin(self.id)].concat();
-        dbg!(&key);
-        let id: Option<u64> = R
-          .fcall_ro(ZMAX, vec![&key[..]], vec!["0"])
+        let id: Option<Vec<u8>> = R
+          .fcall_ro(ZMAX, vec![&key[..]], vec![0])
           .await
           .unwrap_or_else(|err| {
             dbg!(err);
             None
           });
-
+        let id = if let Some(id) = id {
+          Some(bin_u64(id))
+        } else {
+          None
+        };
         self._user_id = id;
         id
       } else {
