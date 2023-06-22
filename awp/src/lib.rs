394a463db20167e3abaab9956b4443dc59529730
xxai.art <xxai.art@gmail.com>
2023-06-23 00:53:14 +0800

    ♨

--- awp/src/lib.rs
+++ awp/src/lib.rs
@@ -6,7 +6,6 @@ mod err;
 mod log;
 mod srv;
 
-use axum::Extension;
 pub use env::env_default;
 pub use err::{Err, Error, Result};
 pub use log::init;
@@ -14,4 +13,23 @@ pub use srv::srv;
 
 pub type Response = Result<axum::response::Response>;
 
+use axum::Extension;
 pub type E<T> = Extension<T>;
+
+use std::convert::Infallible;
+
+use axum::{
+  body::HttpBody,
+  handler::Handler,
+  routing::{post, MethodRouter},
+};
+
+pub fn pack<H, T, S, B>(handler: H) -> MethodRouter<S, B, Infallible>
+where
+  H: Handler<T, S, B>,
+  B: HttpBody + Send + 'static,
+  T: 'static,
+  S: Clone + Send + Sync + 'static,
+{
+  post(handler)
+}
