165e14ad6e8f6d46ba724c8f6bf83dfc3b475fd0
xxai.art <xxai.art@gmail.com>
2023-06-23 20:35:01 +0800

    ♨

--- csdb/src/lib.rs
+++ csdb/src/lib.rs
@@ -1,14 +1,22 @@
 use std::{
   env::{var, VarError},
+  fmt::{Display, Formatter},
   sync::Arc,
 };
 
+use aho_corasick::AhoCorasick;
 use ceresdb_client::{
-  Builder, DbClient, Error, Mode, RpcConfig, RpcContext, SqlQueryRequest, SqlQueryResponse,
+  model::sql_query::row::Row, Builder, DbClient, Error, Mode, RpcConfig, RpcContext,
+  SqlQueryRequest, SqlQueryResponse,
 };
 use coarsetime::Instant;
 use dyn_fmt::AsStrFormatExt;
+use lazy_static::lazy_static;
 use tracing::{error, info};
+
+pub static NONE: [u8; 0] = [];
+
+pub type SQL = Sql<'static>;
 
 pub fn conn_by_env(env: impl AsRef<str>) -> Result<Db, VarError> {
   let grpc = var(env.as_ref())?;
@@ -65,31 +73,85 @@ impl<const N: usize> From<[&str; N]> for Tables {
     Tables(v.map(|i| i.to_string()).into_iter().collect())
   }
 }
+#[derive(Debug, Clone)]
+pub struct Val(String);
+
+impl Display for Val {
+  fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+    write!(f, "{}", self.0)
+  }
+}
+
+macro_rules! into_val {
+    ($($ty:ty),+) => {
+        $(
+            impl From<$ty> for Val {
+                fn from(v: $ty) -> Self {
+                    Val(v.to_string())
+                }
+            }
+            impl From<&$ty> for Val {
+                fn from(v: &$ty) -> Self {
+                    Val(v.to_string())
+                }
+            }
+        )+
+    };
+}
+
+into_val!(u8, u16, u32, u64, u128, usize, i8, i16, i32, i64, i128, isize, f32, f64);
+
+pub static REPLACE: [&str; 4] = ["\\", "\'", "\r", "\n"];
+pub static REPLACE_WITH: [&str; 4] = ["\\\\", "\\\'", "\\r", "\\n"];
+
+lazy_static! {
+  static ref AC: AhoCorasick = AhoCorasick::new(REPLACE).unwrap();
+}
+
+impl From<&str> for Val {
+  fn from(v: &str) -> Self {
+    let mut wtr = vec![];
+    AC.try_stream_replace_all(v.as_bytes(), &mut wtr, &REPLACE_WITH)
+      .unwrap();
+
+    wtr.push(b'\'');
+    wtr.insert(0, b'\'');
+
+    Val(String::from_utf8_lossy(&wtr).into())
+  }
+}
+
+impl From<String> for Val {
+  fn from(v: String) -> Self {
+    v.as_str().into()
+  }
+}
+
+pub struct ValLi(pub Vec<Val>);
+
+impl<T: IntoIterator<Item = V>, V: Into<Val>> From<T> for ValLi {
+  fn from(iter: T) -> Self {
+    ValLi(iter.into_iter().map(|i| i.into()).collect())
+  }
+}
 
 impl<'a> Sql<'a> {
-  pub async fn ignore_err_no_return(&self) {
-    self.ignore_err().await;
+  pub async fn li(&self, args: impl Into<ValLi>) -> Result<Vec<Row>, Error> {
+    Ok(self.exe(args).await?.rows)
   }
 
-  pub async fn ignore_err(&self) -> Option<SqlQueryResponse> {
-    match self.exe().await {
-      Ok(r) => return Some(r),
-      Err(err) => match err {
-        Error::Server(e) => {
-          error!("CERESDB ERROR CODE {}:\n{}\n", e.code, e.msg);
-        }
-        _ => {
-          error!("{err}");
-        }
-      },
-    };
-    None
-  }
-
-  pub async fn exe(&self) -> Result<SqlQueryResponse, Error> {
+  pub async fn exe(&self, args: impl Into<ValLi>) -> Result<SqlQueryResponse, Error> {
     let timer = Instant::now();
     let db = &self.db;
-    let sql = self.sql.to_string();
+
+    let args = args.into().0;
+
+    let sql = if args.len() > 0 {
+      self.sql.format(&args)
+    } else {
+      self.sql.to_string()
+    };
+
     let req = SqlQueryRequest {
       tables: self.tables.clone(),
       sql,
@@ -97,6 +159,16 @@ impl<'a> Sql<'a> {
     let r = db.client.sql_query(&db.ctx, &req).await;
     let cost = timer.elapsed().as_millis();
     info!("{}ms\n{}", cost, &self.sql);
+    if let Err(err) = &r {
+      match err {
+        Error::Server(e) => {
+          error!("CERESDB ERROR CODE {}:\n{}\n", e.code, e.msg);
+        }
+        _ => {
+          error!("{err}");
+        }
+      }
+    }
     r
   }
 }
--- init/src/main.rs
+++ init/src/main.rs
@@ -1,13 +1,13 @@
-use csdb::{conn_by_env, Db, Sql};
+use csdb::{conn_by_env, Db, NONE, SQL};
 use lazy_static::lazy_static;
 
 lazy_static! {
     pub static ref DB: Db = conn_by_env("CERESDB_GRPC").unwrap();
-    pub static ref SQL_DROP_FAV: Sql<'static> = DB.sql(["fav"], "DROP TABLE fav");
+    pub static ref SQL_DROP_FAV: SQL = DB.sql(["fav"], "DROP TABLE fav");
 
     // ctime 是用户记录创建时间
     // ts 是写入时间
-    pub static ref SQL_FAV: Sql<'static> = DB.sql(["fav"], r#"CREATE TABLE fav (
+    pub static ref SQL_FAV: SQL = DB.sql(["fav"], r#"CREATE TABLE fav (
   ts TIMESTAMP NOT NULL,
   ctime uint64 NOT NULL,
   uid uint64 NOT NULL,
@@ -20,15 +20,24 @@ lazy_static! {
   compression='ZSTD',
   enable_ttl='false'
 )"#);
-
+    pub static ref SQL_INSERT: SQL = DB.sql(["fav"], "INSERT INTO fav (ts,ctime,uid,action,kind,rid) VALUES ({},{},{},{},{},{})");
+    pub static ref SQL_SELECT: SQL = DB.sql(["fav"], "SELECT * FROM fav");
+    // pub static ref SQL_DELETE: SQL = DB.sql(["fav"], "DELETE FROM fav WHERE ts={} AND uid={}");
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
   loginit::init();
 
-  SQL_DROP_FAV.ignore_err_no_return().await;
-  SQL_FAV.ignore_err_no_return().await;
+  let _ = SQL_DROP_FAV.exe(NONE).await;
+  SQL_FAV.exe(NONE).await?;
+  SQL_INSERT.exe([1, 2, 3, 4, 5, 6]).await?;
+  SQL_INSERT.exe([2, 2, 3, 4, 5, 6]).await?;
 
+  let li = SQL_SELECT.li(NONE).await?;
+  for i in li {
+    dbg!(&i);
+  }
+  // SQL_DELETE.exe([1, 3]).await?;
   Ok(())
 }
