145810891d16288a95441381be299f015deeb282
xxai.art <xxai.art@gmail.com>
2023-06-24 10:56:17 +0800

    ♨

--- rsrv/src/csdb.rs
+++ rsrv/src/csdb.rs
@@ -1,3 +1,5 @@
 lazy_static! {
   pub static ref CSDB: Db = conn_by_env("CERESDB_GRPC").unwrap();
+  pub static ref SQL_INSERT: SQL =
+    DB.sql("INSERT INTO fav (ts,ctime,uid,action,kind,rid) VALUES ({},{},{},{},{},{})");
 }
