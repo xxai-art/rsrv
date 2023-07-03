1c7d27f2e9eefdbbe1d53465719f946c68253277
xxai.art <xxai.art@gmail.com>
2023-07-03 17:06:58 +0800

    ♨

--- rsrv/src/url/fav.rs
+++ rsrv/src/url/fav.rs
@@ -5,17 +5,9 @@ use xxpg::Q01;
 
 #[derive(Serialize, Debug, Deserialize)]
 struct FavSync(u64, Vec<(u16, u64, u64, i8)>);
-// use crate::cs::FAV_INSERT;
-
-// use axum::extract::Host;
-// use client::client;
-// use tower_cookies::Cookies;
-//use x0::R;
-
-// use anypack::url_fn;
 
 Q01!(
-  fav_user:
+    fav_user:
     INSERT INTO fav.user (user_id,cid,rid,ctime,action) VALUES ($1,$2,$3,$4,$5) ON CONFLICT (user_id, cid, rid, ctime) DO NOTHING RETURNING id
 );
 
@@ -24,42 +16,14 @@ pub async fn post(mut client: Client, body: Bytes) -> awp::any!() {
     serde_json::from_str(unsafe { std::str::from_utf8_unchecked(&body) })?;
 
   let mut id = 0;
-  // let mut id_li = Vec::<Vec<u8>>::with_capacity(fav_li.len());
+  let mut n = 0;
   if client.is_login(user_id).await? {
-    //{cid: 2, rid: 215060, ctime: 1688364595987, action: 0}
     for (cid, rid, ctime, action) in fav_li {
       if let Some(_id) = fav_user(&user_id, &cid, &rid, &ctime, &action).await? {
-        id = _id
+        id = _id;
+        n += 1;
       }
-      //   // id_li.push(vbyte::compress_list(&[cid as u64, rid]));
     }
-
-    // fav_user(
-    //   fav_li
-    //     .into_iter()
-    //     .map(|(cid, rid, ctime, action)| format!("({user_id},{cid},{rid},{ctime},{action})",))
-    //     .collect(),
-    // )
-    // .await?;
   }
-  // let user_id: u64 = body[0].into();
-
-  // dbg!(client.user_id().await?);
-  // dbg!(client);
-  //输出0-1
-  // CS.sql("INSERT INTO fav (ts,ctime,uid,action,kind,rid) VALUES ({},{},{},{},{},{})");
-  //FAV_INSERT.exe().await;
-  // let mut ts = coarsetime::Clock::now_since_epoch().as_millis();
-  // let mut ctime = ts;
-  //
-  // dbg!(body);
-  // FAV_INSERT.exe(
-  //   ts,
-  //   ctime,
-  //   uid,
-  //   FAV_ACTION_NEW,
-  //   FAV_ACTION_IMG,
-  //   rid
-  // ).await;
   Ok(id)
 }
--- x0/src/lib.rs
+++ x0/src/lib.rs
@@ -2,3 +2,4 @@ use xkv::conn;
 pub use xkv::fred;
 
 conn!(R = REDIS);
+conn!(KV = KV);
