a59214652bac93c5cc8c3ffb2eb38a0f4e2c40c6
xxai.art <xxai.art@gmail.com>
2023-07-04 17:19:37 +0800

    ♨

--- rsrv/src/url/es.rs
+++ rsrv/src/url/es.rs
@@ -11,10 +11,12 @@ use crate::es;
 
 pub async fn get(mut client: Client, Path(li): Path<String>) -> awp::Result<Response> {
   let li = xxai::b64_u64_li(li);
-  if li.len() >= 3 {
+  if li.len() >= 4 {
     let user_id = li[0];
+    let last_event_id = li[1];
+
     if client.is_login(user_id).await? {
-      let _li = &li[1..];
+      let _li = &li[2..];
 
       let client_id = u64_bin(client.id);
 
@@ -30,7 +32,11 @@ pub async fn get(mut client: Client, Path(li): Path<String>) -> awp::Result<Resp
 
       let channel_id = xxai::b64(client_id);
 
-      let url = format!("/nchan/{}", channel_id);
+      let mut url = format!("/nchan/{}", channel_id);
+
+      if last_event_id > 0 {
+        url = format!("{url}?last_event_id={last_event_id}:0");
+      }
 
       tokio::spawn(async move {
         es::publish_b64(channel_id, "good s");
