aeee288e74ffcb6f705f568b6f5d0029888b8d60
xxai.art <xxai.art@gmail.com>
2023-07-05 16:57:04 +0800

    ♨

--- rsrv/src/url/es.rs
+++ rsrv/src/url/es.rs
@@ -13,18 +13,20 @@ use xxpg::Q;
 
 use crate::{es, K};
 
-const LIMIT: u64 = 2;
+const LIMIT: usize = 2;
+const EVENT_SYNC: u64 = 1;
 
 Q!(
 
-fav_li:
-  SELECT id,cid,rid,ctime,action FROM fav.user WHERE user_id=$1 AND id>$2 ORDER BY id LIMIT 2
+    fav_li:
+    SELECT id,cid,rid,ctime,action FROM fav.user WHERE user_id=$1 AND id>$2 ORDER BY id LIMIT 2
 
 );
 
 macro_rules! es_sync {
   ($user_id:expr, $channel_id: expr, $li: expr) => {
     tokio::spawn(async move {
+      let channel_id = $channel_id;
       let user_id = $user_id;
       let p = KV.pipeline();
       p.hincrby(K::FAV_ID, user_id, 0).await?;
@@ -33,14 +35,29 @@ macro_rules! es_sync {
 
       let fav_synced_id = $li[0];
       let fav_synced = $li[1];
+      let mut n = 0;
 
       if fav_synced_id < r[0] {
-        let mut n = 0;
         let mut id = fav_synced_id;
-        fav_li(&user_id, &fav_synced_id).await?;
+        loop {
+          let fav_li = fav_li(&user_id, &id).await?;
+          let len = fav_li.len();
+          n += len;
+          if len > 0 {
+            let mut json = String::new();
+            for i in &fav_li {
+              json += &format!("{},{},{},{},", i.1, i.2, i.3, i.4);
+            }
+            let json = &json[..json.len() - 1];
+            es::publish_b64(&channel_id, format!("[{user_id},{EVENT_SYNC},{json}]"));
+          }
+          if len != LIMIT {
+            break;
+          }
+          id = fav_li.last().unwrap().0;
+        }
       }
 
-      es::publish_b64($channel_id, "good s");
       Ok::<_, anyhow::Error>(())
     });
   };
