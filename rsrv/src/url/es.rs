73c52c7adee51921afa02c19fb294a59b1a14072
xxai.art <xxai.art@gmail.com>
2023-07-05 16:21:13 +0800

    ♨

--- rsrv/src/url/es.rs
+++ rsrv/src/url/es.rs
@@ -4,22 +4,30 @@ use axum::{
   response::{IntoResponse, Response},
 };
 use client::Client;
-use x0::{fred::interfaces::SortedSetsInterface, KV};
+use x0::{
+  fred::interfaces::{HashesInterface, SortedSetsInterface},
+  KV,
+};
 use xxai::u64_bin;
 
 use crate::{es, K};
 
 macro_rules! es_sync {
-  ($channel_id: expr, $li: expr) => {
+  ($user_id:expr, $channel_id: expr, $li: expr) => {
     tokio::spawn(async move {
       let fav_synced = $li[0];
       let fav_synced_id = $li[1];
       dbg!(fav_synced, fav_synced_id);
       let p = KV.pipeline();
+      p.hincrby(K::FAV_ID, $user_id, 0).await?;
+      p.hincrby(K::FAV_SUM, $user_id, 0).await?;
+      let r: Vec<u64> = p.all().await?;
+      dbg!(r);
 
       dbg!(String::from_utf8_lossy(K::FAV_SUM));
       // TODO KV.hmget
       es::publish_b64($channel_id, "good s");
+      Ok::<_, anyhow::Error>(())
     });
   };
 }
@@ -46,7 +54,7 @@ pub async fn get(mut client: Client, Path(li): Path<String>) -> awp::Result<Resp
 
       let url = format!("/nchan/{}", channel_id);
 
-      es_sync!(channel_id, &li[1..]);
+      es_sync!(user_id, channel_id, &li[1..]);
 
       return Ok(
         (
