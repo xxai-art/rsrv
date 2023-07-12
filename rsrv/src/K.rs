bd8f428c147912d781e6a4dc44f581e7e29fc38d
xxai.art <xxai.art@gmail.com>
2023-07-12 17:26:07 +0800

    ♨

--- rsrv/src/K.rs
+++ rsrv/src/K.rs
@@ -1,14 +1,14 @@
 use xxai::u64_bin;
 
-macro_rules! key {
-    ($($key:ident),+) => {
-        $(
-            paste! {
-                pub static [<$key:snake:upper>]: &'static [u8] = stringify!($key).as_bytes();
-            }
-        )+
-    };
-}
+// macro_rules! key {
+//     ($($key:ident),+) => {
+//         $(
+//             paste! {
+//                 pub static [<$key:snake:upper>]: &'static [u8] = stringify!($key).as_bytes();
+//             }
+//         )+
+//     };
+// }
 
 // key!(favSum, favId);
 
--- rsrv/src/url/fav.rs
+++ rsrv/src/url/fav.rs
@@ -21,79 +21,79 @@ fav_li:
 );
 
 pub async fn fav_batch_add(
-  prev_id: u64,
-  client_id: u64,
-  uid: u64,
-  fav_li: Vec<(u16, u64, u64, i8)>,
+    prev_id: u64,
+    client_id: u64,
+    uid: u64,
+    fav_li: Vec<(u16, u64, u64, i8)>,
 ) -> anyhow::Result<u64> {
-  let mut id = 0;
-  let mut n = 0;
-  let mut json = String::new();
-  // batch_insert!(
-  //   "INSERT INTO fav.user (uid,cid,rid,ts,aid) VALUES {} ON CONFLICT (uid, cid, rid, ts) DO NOTHING RETURNING id",
-  //   fav_li.into_iter().map(|x|( uid,x.0,x.1,x.2,x.3 )).collect::<Vec<_>>()
-  // );
-  for (cid, rid, ts, aid) in fav_li {
-    if let Some(_id) = fav_user(uid, cid, rid, ts, aid).await? {
-      id = _id;
-      n += 1;
-      json += &format!("{cid},{rid},{ts},{aid},");
+    let mut id = 0;
+    let mut n = 0;
+    let mut json = String::new();
+    // batch_insert!(
+    //   "INSERT INTO fav.user (uid,cid,rid,ts,aid) VALUES {} ON CONFLICT (uid, cid, rid, ts) DO NOTHING RETURNING id",
+    //   fav_li.into_iter().map(|x|( uid,x.0,x.1,x.2,x.3 )).collect::<Vec<_>>()
+    // );
+    for (cid, rid, ts, aid) in fav_li {
+        if let Some(_id) = fav_user(uid, cid, rid, ts, aid).await? {
+            id = _id;
+            n += 1;
+            json += &format!(",{cid},{rid},{ts},{aid}");
+        }
     }
-  }
-  if n > 0 {
-    // let p = KV.pipeline();
-    // p.hincrby(K::FAV_SUM, uid, n).await?;
-    // p.hset(K::FAV_ID, (uid, id)).await?;
-    // p.all().await?;
-    publish_to_user_client(
-      client_id,
-      uid,
-      KIND_SYNC_FAV,
-      format!("{json},{prev_id},{id}"),
-    );
-  }
-  Ok(id)
+    if n > 0 {
+        // let p = KV.pipeline();
+        // p.hincrby(K::FAV_SUM, uid, n).await?;
+        // p.hset(K::FAV_ID, (uid, id)).await?;
+        // p.all().await?;
+        publish_to_user_client(
+            client_id,
+            uid,
+            KIND_SYNC_FAV,
+            format!("{prev_id},{id},{json}"),
+        );
+    }
+    Ok(id)
 }
 
 pub async fn post(client: Client, body: Bytes) -> awp::any!() {
-  let mut r = Vec::new();
-  let li: Vec<u64> = serde_json::from_str(unsafe { std::str::from_utf8_unchecked(&body) })?;
-  if li.len() > 2 {
-    let uid = li[0];
-    if client.is_login(uid).await? {
-      let last_sync_id = li[1];
-      let li: Vec<_> = li[2..]
-        .chunks_exact(4)
-        .map(|i| (i[0] as u16, i[1], i[2], i[3] as i8))
-        .collect();
+    let mut r = Vec::new();
+    let li: Vec<u64> = serde_json::from_str(unsafe { std::str::from_utf8_unchecked(&body) })?;
+    if li.len() > 2 {
+        let uid = li[0];
+        if client.is_login(uid).await? {
+            let last_sync_id = li[1];
+            let li: Vec<_> = li[2..]
+                .chunks_exact(4)
+                .map(|i| (i[0] as u16, i[1], i[2], i[3] as i8))
+                .collect();
 
-      for i in &li {
-        fav_rm(uid, i.0, i.1).await?
-      }
+            for i in &li {
+                fav_rm(uid, i.0, i.1).await?
+            }
 
-      let fav_li = fav_li(uid, last_sync_id).await?;
-      let mut id = 0;
-      if !fav_li.is_empty() {
-        for i in fav_li {
-          id = i.0;
-          r.push(i.1 as u64);
-          r.push(i.2);
-          r.push(i.3);
-          r.push(i.4 as u64);
-        }
-      }
+            let fav_li = fav_li(uid, last_sync_id).await?;
+            let mut id = 0;
+            if !fav_li.is_empty() {
+                for i in fav_li {
+                    id = i.0;
+                    r.push(i.1 as u64);
+                    r.push(i.2);
+                    r.push(i.3);
+                    r.push(i.4 as u64);
+                }
+            }
 
-      let _id = fav_batch_add(id, client.id, uid, li).await?;
-      if _id != 0 {
-        id = _id;
-      }
+            let _id = fav_batch_add(id, client.id, uid, li).await?;
+            if _id != 0 {
+                id = _id;
+            }
 
-      if id != 0 {
-        r.push(id);
-      }
-    };
-  }
-  Ok(r)
+            if id != 0 {
+                r.push(id);
+            }
+        };
+    }
+    Ok(r)
 }
 
 // macro_rules! batch_insert {
