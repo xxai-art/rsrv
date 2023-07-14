8614a095e8a476ea7032db1993b101ba5828b3eb
xxai.art <xxai.art@gmail.com>
2023-07-15 07:53:18 +0800

    ♨

--- rsrv/src/url/q.rs
+++ rsrv/src/url/q.rs
@@ -1,6 +1,10 @@
 use awp::{any, ok};
+use axum::body::Bytes;
 
-pub async fn post() -> any!() {
+pub async fn post(body: Bytes) -> any!() {
   // ok!()
+  let q_dl_dt: (String, u8, u8) =
+    serde_json::from_str(unsafe { std::str::from_utf8_unchecked(&body) })?;
+  dbg!(q_dl_dt);
   Ok(0)
 }
