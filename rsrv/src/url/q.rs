27ae5cfac934e6f23c716fd1aa8eae0a4bf9bcb6
xxai.art <xxai.art@gmail.com>
2023-07-15 08:10:42 +0800

    ♨

--- rsrv/src/url/q.rs
+++ rsrv/src/url/q.rs
@@ -3,8 +3,19 @@ use axum::body::Bytes;
 
 pub async fn post(body: Bytes) -> any!() {
   // ok!()
-  let q_dl_dt: (String, u8, u8) =
-    serde_json::from_str(unsafe { std::str::from_utf8_unchecked(&body) })?;
-  dbg!(q_dl_dt);
+  let body = &body[1..body.len() - 1];
+  for (pos, i) in body.iter().enumerate() {
+    if *i == b'/' {
+      let args = xxai::b64_u64_li(&body[..pos]);
+      if args.len() >= 3 {
+        let rating = args[0];
+        let begin_time = args[1];
+        let duration = args[2];
+        let q = String::from_utf8_lossy(&body[1 + pos..]);
+        dbg!(rating, begin_time, duration, q);
+      }
+      break;
+    }
+  }
   Ok(0)
 }
