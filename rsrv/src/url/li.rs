9ca24accba9e85c9b98d4e2f22098ba3353d77b5
xxai.art <xxai.art@gmail.com>
2023-07-18 15:02:10 +0800

    ♨

--- rsrv/src/url/li.rs
+++ rsrv/src/url/li.rs
@@ -3,7 +3,7 @@ use xxpg::Q;
 
 Q!(
     img_li:
-    SELECT task.id,hash::bytea,w,h,laugh FROM bot.task,bot.civitai_img WHERE hash IS NOT NULL AND bot.task.rid=bot.civitai_img.id AND task.adult=0 AND cid=1 ORDER BY star DESC LIMIT 600
+    SELECT task.id FROM bot.task,bot.civitai_img WHERE hash IS NOT NULL AND bot.task.rid=bot.civitai_img.id AND task.adult=0 AND cid=1 ORDER BY star DESC LIMIT 600
 );
 
 pub async fn post() -> any!() {
