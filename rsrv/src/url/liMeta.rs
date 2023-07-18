51755830ac02307d51d2493f6be84046112699e1
xxai.art <xxai.art@gmail.com>
2023-07-18 14:02:17 +0800

    ♨

--- rsrv/src/url/liMeta.rs
+++ rsrv/src/url/liMeta.rs
@@ -7,44 +7,6 @@ use xxai::u64_bin;
 
 use crate::cid::CID_IMG;
 
-//
-// #[derive(Serialize, Debug, Deserialize)]
-// struct FavSync(u64, Vec<(u16, u64, u64, i8)>);
-//
-// Q01!(
-// fav_user:
-// INSERT INTO fav.user (uid,cid,rid,ts,aid) VALUES ($1,$2,$3,$4,$5) ON CONFLICT (uid, cid, rid, ts) DO NOTHING RETURNING id
-// );
-//
-// pub async fn fav_batch_add(
-//   client_id: u64,
-//   uid: u64,
-//   fav_li: Vec<(u16, u64, u64, i8)>,
-// ) -> anyhow::Result<u64> {
-//   let mut id = 0;
-//   let mut n = 0;
-//   let mut json = String::new();
-//   // batch_insert!(
-//   //   "INSERT INTO fav.user (uid,cid,rid,ts,aid) VALUES {} ON CONFLICT (uid, cid, rid, ts) DO NOTHING RETURNING id",
-//   //   fav_li.into_iter().map(|x|( uid,x.0,x.1,x.2,x.3 )).collect::<Vec<_>>()
-//   // );
-//   for (cid, rid, ts, aid) in fav_li {
-//     if let Some(_id) = fav_user(uid, cid, rid, ts, aid).await? {
-//       id = _id;
-//       n += 1;
-//       json += &format!("{cid},{rid},{ts},{aid},");
-//     }
-//   }
-//   if n > 0 {
-//     let p = KV.pipeline();
-//     p.hincrby(K::FAV_SUM, uid, n).await?;
-//     p.hset(K::FAV_ID, (uid, id)).await?;
-//     p.all().await?;
-//     publish_to_user_client(client_id, uid, KIND_SYNC_FAV, format!("{json}{id}"));
-//   }
-//   Ok(id)
-// }
-//
 pub async fn post(_client: Client, body: Bytes) -> awp::any!() {
   let r: Any;
   if let Some(first) = body.first() {
@@ -79,15 +41,18 @@ pub async fn post(_client: Client, body: Bytes) -> awp::any!() {
           match *cid {
             CID_IMG => {
               let key_map: Vec<_> = li.into_iter().map(|i| u64_bin(*i)).collect();
+              dbg!(111);
               let vli: Vec<Option<Vec<u8>>> = KV.hmget("liMeta", key_map).await?;
+              dbg!(11122);
 
-              for i in vli {
-                if let Some(i) = i {
-                  tli.push(i)
-                } else {
-                  tli.push(Any::Null)
-                }
-              }
+              // for i in vli {
+              //   if let Some(i) = i {
+              //     tli.push(i)
+              //   } else {
+              //     tli.push(Any::Null)
+              //   }
+              // }
+              tli.push(Any::Null)
             }
             _ => {}
           }
