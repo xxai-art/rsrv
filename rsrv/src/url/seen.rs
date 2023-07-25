f821cebaca0669f7de296bf45876e879e163b320
xxai.art <xxai.art@gmail.com>
2023-07-25 14:24:38 +0800

    ♨

--- rsrv/src/url/seen.rs
+++ rsrv/src/url/seen.rs
@@ -100,7 +100,12 @@ pub async fn post(client: Client, body: Bytes) -> awp::any!() {
             .await?;
             set_last(K::SEEN_LAST, uid, ts);
             let to_publish = to_publish.join(",");
-            publish_to_user_client(client.id, uid, KIND_SYNC_SEEN, format!("[-1,{to_publish}]"));
+            publish_to_user_client(
+              client.id,
+              uid,
+              KIND_SYNC_SEEN,
+              format!("[{ts},{to_publish}]"),
+            );
             r.push(ts);
           }
         }
