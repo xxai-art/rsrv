c55bbca21249987a3d5c79e50b0ab176a52b8a40
xxai.art <xxai.art@gmail.com>
2023-07-25 13:48:05 +0800

    ♨

--- rsrv/src/db/seen.rs
+++ rsrv/src/db/seen.rs
@@ -13,18 +13,18 @@ PRIMARY KEY (uid, cid, rid)
 */
 
 pub fn after_ts_sql(uid: u64, ts: u64) -> String {
-  format!("SELECT cid,rid,CAST(ts as BIGINT) t FROM seen WHERE uid={uid} AND ts>{ts} ORDER BY TS")
+  format!("SELECT CAST(ts as BIGINT) t,cid,rid FROM seen WHERE uid={uid} AND ts>{ts} ORDER BY TS")
 }
 
 pub async fn after_ts(sql: impl AsRef<str>) -> Result<Vec<u64>> {
   let mut r = Vec::new();
   for i in GQ(sql.as_ref(), &[]).await? {
-    let cid: i8 = i.get(0);
-    r.push(cid as u64);
-    let rid: i64 = i.get(1);
-    r.push(rid as u64);
-    let ts: i64 = i.get(2);
+    let ts: i64 = i.get(0);
     r.push(ts as u64);
+    let cid: i8 = i.get(1);
+    r.push(cid as u64);
+    let rid: i64 = i.get(2);
+    r.push(rid as u64);
   }
   Ok(r)
 }
