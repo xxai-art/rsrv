dedcec763f17d55367ec33aeee61b4294de5df6b
xxai.art <xxai.art@gmail.com>
2023-07-25 12:38:54 +0800

    ♨

--- rsrv/src/url/es.rs
+++ rsrv/src/url/es.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use axum::{
   extract::Path,
   http::StatusCode,
@@ -10,8 +11,7 @@ use xxai::u64_bin;
 use xxpg::Q;
 
 use crate::{
-  es,
-  es::KIND_SYNC_FAV,
+  db, es,
   kv::sync::{has_more, set_last},
   K,
 };
@@ -22,7 +22,28 @@ Q! {
     fav_li:SELECT id,cid,rid,ts,aid FROM fav.user WHERE uid=$1 AND id>$2 ORDER BY id LIMIT 2048;
 }
 
-// fav_ym_n: "SELECT TO_CHAR(to_timestamp(cast(ts/1000 as u64)) AT TIME ZONE 'UTC','YYYYMM')::u64 AS ym, COUNT(1)::u64 FROM fav.user WHERE uid=$1 GROUP BY ym"
+async fn seen_li(uid: u64, ts: u64) -> Result<Vec<u64>> {
+  let sql = &format!("SELECT cid,rid,CAST(ts as BIGINT) t FROM seen WHERE uid={uid} AND ts>CAST({ts} as TIMESTAMP) ORDER BY ts LIMIT 3");
+  db::seen::after_ts(sql).await
+}
+
+macro_rules! last_0 {
+  ($last:expr) => {
+    $last.0
+  };
+}
+
+macro_rules! last {
+  ($last:expr) => {
+    *$last
+  };
+}
+
+macro_rules! json_seen {
+  ($i:expr) => {{
+    format!(",{}", $i)
+  }};
+}
 
 macro_rules! json_fav {
   ($i:expr) => {{
@@ -32,49 +53,56 @@ macro_rules! json_fav {
 }
 
 macro_rules! es_sync {
-  ($uid:expr, $channel_id: expr, $prev_id: expr, $key:ident) => {
+  ($uid:expr, $channel_id: expr, $prev_id: expr, $key:ident, $last:ident) => {{
+    let channel_id = $channel_id.clone();
+    let prev_id = $prev_id;
+    let uid = $uid;
     trt::spawn!({
-      let channel_id = $channel_id;
-      let uid = $uid;
       let uid_bin = u64_bin(uid);
       paste! {
-      let last_key = K::[< $key:upper _LAST >];
-      if let Some(last_id) = has_more(last_key, &uid_bin, $prev_id).await? {
-        let mut id = $prev_id;
-        loop {
-          let prev_id = id;
-          let li = [<$key _li>](uid, id).await?;
-          let len = li.len();
-          if len > 0 {
-            id = li.last().unwrap().0;
-            let mut json = String::new();
-            for i in &li {
-              json += &[<json_ $key>]!(i);
-            }
-            es::publish_b64(
-              &channel_id,
-              [<KIND_SYNC_ $key:upper>],
-              format!("{uid},{prev_id},{id}{json}"),
-            )
-            .await?;
+          let last_key = K::[< $key:upper _LAST >];
+          if let Some(last_id) = has_more(last_key, &uid_bin, prev_id).await? {
+              let mut id = prev_id;
+              loop {
+                  let prev_id = id;
+                  let li = [<$key _li>](uid, id).await?;
+                  let len = li.len();
+                  if len > 0 {
+                      id = $last!(li.last().unwrap());
+                      let mut json = String::new();
+                      for i in &li {
+                          json += &[<json_ $key>]!(i);
+                      }
+                      es::publish_b64(
+                          &channel_id,
+                          es::[<KIND_SYNC_ $key:upper>],
+                          format!("{uid},{prev_id},{id}{json}"),
+                      )
+                          .await?;
+                  }
+                  if len != LIMIT {
+                      break;
+                  }
+              }
+              if id != last_id {
+                  set_last(last_key, uid, id);
+              }
           }
-          if len != LIMIT {
-            break;
-          }
-        }
-        if id != last_id {
-          set_last(last_key, uid, id);
-        }
-      }
       }
     });
-  };
+  }};
 }
 
 macro_rules! es_sync_li {
-  ($uid:expr, $channel_id: expr, $li: expr) => {
-    es_sync!($uid, $channel_id, $li[0], fav)
+  ($uid:expr, $channel_id: expr, $li:ident: $($pos:expr,$key:ident,$last:ident);*) => {
+    $(
+      es_sync!($uid, $channel_id, $li[$pos], $key, $last);
+    )*
   };
+  ($uid:expr, $channel_id: expr, $li: expr) => {{
+    let li = $li;
+    es_sync_li!($uid, $channel_id, li: 0, fav, last_0; 1, seen, last);
+  }}
 }
 
 pub async fn get(client: Client, Path(li): Path<String>) -> awp::Result<Response> {
