9e2f232dd8eae626dd742445b11ee29d35f718d8
xxai.art <xxai.art@gmail.com>
2023-08-17 10:17:52 +0800

    ♨

--- pgw/tests/main.rs
+++ pgw/tests/main.rs
@@ -5,7 +5,7 @@ use tokio::time;
 async fn main() -> anyhow::Result<()> {
   loginit::init();
   let pg = Pg::new("PG_URI");
-  for i in 0..9999999 {
+  for i in 0..99999 {
     match pg
       .query("SELECT nspname FROM pg_catalog.pg_namespace LIMIT 1", &[])
       .await
