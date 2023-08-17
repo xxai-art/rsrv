e14c270ab7813cc09a56a8ddbc2e121d2a54937d
xxai.art <xxai.art@gmail.com>
2023-08-17 19:08:28 +0800

    ♨

---
+++ xxpg/tests/main.rs
@@ -0,0 +1,12 @@
+use xxpg::Q;
+Q! {
+    li:
+        SELECT cid,rid,ts FROM fav.user ORDER BY ts DESC LIMIT 10
+}
+
+#[tokio::test]
+async fn main() -> anyhow::Result<()> {
+  let r = li().await?;
+  dbg!(r);
+  Ok(())
+}
