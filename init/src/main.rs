4253933fbb597d696e881f2805428c62e92fe828
xxai.art <xxai.art@gmail.com>
2023-08-19 12:48:24 +0800

    ♨

--- init/src/main.rs
+++ init/src/main.rs
@@ -3,7 +3,8 @@ use gt::GE;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let sql_li = "CREATE TABLE IF NOT EXISTS seen (
+  let sql_li = r#"
+CREATE TABLE IF NOT EXISTS seen (
 uid BIGINT UNSIGNED NOT NULL,
 cid TINYINT UNSIGNED NOT NULL,
 rid BIGINT UNSIGNED NOT NULL,
@@ -11,6 +12,17 @@ ts TIMESTAMP(3) NOT NULL,
 TIME INDEX (ts),
 PRIMARY KEY (uid, cid, rid)
 );
+
+CREATE TABLE IF NOT EXISTS log_n (
+aid TINYINT UNSIGNED NOT NULL,
+cid TINYINT UNSIGNED NOT NULL,
+rid BIGINT UNSIGNED NOT NULL,
+n BIGINT UNSIGNED NOT NULL,
+ts TIMESTAMP(3) NOT NULL,
+TIME INDEX (ts),
+PRIMARY KEY (aid, cid, rid)
+);
+
 CREATE TABLE IF NOT EXISTS log (
 uid BIGINT UNSIGNED NOT NULL,
 aid TINYINT UNSIGNED NOT NULL,
@@ -21,16 +33,19 @@ ts TIMESTAMP(3) NOT NULL,
 TIME INDEX (ts),
 PRIMARY KEY (uid, aid, cid, rid, q)
 );
+
 CREATE TABLE IF NOT EXISTS q (
 id TIMESTAMP(0) NOT NULL,
 q STRING NOT NULL,
 TIME INDEX (id),
 PRIMARY KEY (q)
-);"#,
-];
-for sql in sql_li {
-    println!("{}", sql);
-    GE(sql, &[]).await?;
-}
-Ok(())
+);"#;
+  for sql in sql_li.split(";") {
+    if !sql.is_empty() {
+      let sql = sql.to_owned() + ";";
+      println!("{}", sql);
+      GE(sql, &[]).await?;
+    }
+  }
+  Ok(())
 }
