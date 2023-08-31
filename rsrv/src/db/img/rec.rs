04f580e976561213e18fb3f470cf4b6f12217d81
xxai.art <xxai.art@gmail.com>
2023-08-31 14:36:12 +0800

    ♨

--- rsrv/src/db/img/rec.rs
+++ rsrv/src/db/img/rec.rs
@@ -12,51 +12,51 @@ use crate::K;
 // );
 
 pub async fn rec(z85: &Vec<u64>) -> Result<Vec<u64>> {
-  let level = z85[0];
-  let offset = z85[1];
-  let key = if level == 2 {
-    // 成人
-    K::REC1
-  } else if level == 1 {
-    // 不限
-    K::REC
-  } else {
-    // 安全
-    K::REC0
-  };
-  Ok(li(key, offset).await?)
+    let level = z85[0];
+    let offset = z85[1];
+    let key = if level == 2 {
+        // 成人
+        K::REC1
+    } else if level == 1 {
+        // 不限
+        K::REC
+    } else {
+        // 安全
+        K::REC0
+    };
+    Ok(li(key, offset).await?)
 }
 
 pub async fn li(key: &[u8], offset: u64) -> Result<Vec<u64>> {
-  let offset = offset as i64;
-  let bin_li: Vec<Vec<u8>> = KV.zrevrange(key, offset, offset + 128, false).await?;
-  let li: Vec<u64> = bin_li
-    .iter()
-    .map(|i| match vb::d(i) {
-      Ok(i) => Ok([i[0] as u64, i[1]]),
-      Err(err) => Err(err),
-    })
-    .filter_map(Result::ok)
-    .flat_map(|v| v)
-    .collect();
-  // let li = vec![];
-  // let nsfw_li: Vec<bool> = KV
-  //   .smismember(
-  //     K::NSFW,
-  //     bin_li
-  //       .into_iter()
-  //       .map(|i| {
-  //         let i: RedisValue = (&i[..]).into();
-  //         i
-  //       })
-  //       .collect::<Vec<_>>(),
-  //   )
-  //   .await?;
-  // let mut r = Vec::with_capacity(li.len());
-  // for (id, nsfw) in li.into_iter().zip(nsfw_li) {
-  //   if !nsfw {
-  //     r.push(id);
-  //   }
-  // }
-  Ok(li)
+    let offset = offset as i64;
+    let bin_li: Vec<Vec<u8>> = KV.zrevrange(key, offset, offset + 1023, false).await?;
+    let li: Vec<u64> = bin_li
+        .iter()
+        .map(|i| match vb::d(i) {
+            Ok(i) => Ok([i[0] as u64, i[1]]),
+            Err(err) => Err(err),
+        })
+        .filter_map(Result::ok)
+        .flat_map(|v| v)
+        .collect();
+    // let li = vec![];
+    // let nsfw_li: Vec<bool> = KV
+    //   .smismember(
+    //     K::NSFW,
+    //     bin_li
+    //       .into_iter()
+    //       .map(|i| {
+    //         let i: RedisValue = (&i[..]).into();
+    //         i
+    //       })
+    //       .collect::<Vec<_>>(),
+    //   )
+    //   .await?;
+    // let mut r = Vec::with_capacity(li.len());
+    // for (id, nsfw) in li.into_iter().zip(nsfw_li) {
+    //   if !nsfw {
+    //     r.push(id);
+    //   }
+    // }
+    Ok(li)
 }
