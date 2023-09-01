b8dfcd573a10942f3cadbc069ad223113d330995
xxai.art <xxai.art@gmail.com>
2023-09-01 10:40:14 +0800

    ♨

--- init/src/main.rs
+++ init/src/main.rs
@@ -1,6 +1,21 @@
 use anyhow::Error;
 use gt::QE;
 
+fn remove_comment<S: AsRef<str>>(input: S) -> String {
+  let lines = input.as_ref().lines();
+  let filtered_lines: Vec<String> = lines
+    .map(|line| {
+      if let Some(pos) = line.find('#') {
+        String::from(line[0..pos].trim_end())
+      } else {
+        String::from(line)
+      }
+    })
+    .filter(|line| !line.is_empty())
+    .collect();
+  filtered_lines.join("\n")
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
   let sql_li = r#"
@@ -23,6 +38,18 @@ TIME INDEX (ts),
 PRIMARY KEY (aid, cid, rid)
 );
 
+# 推荐的点击链
+CREATE TABLE IF NOT EXISTS rec_chain (
+uid BIGINT UNSIGNED NOT NULL,
+aid TINYINT UNSIGNED NOT NULL,
+cid TINYINT UNSIGNED NOT NULL,
+rid BIGINT UNSIGNED NOT NULL,
+rel BIGINT UNSIGNED NOT NULL, # 推荐来源 relationship
+ts TIMESTAMP(3) NOT NULL,
+TIME INDEX (ts),
+PRIMARY KEY (uid, aid, cid, rid, rel)
+);
+
 CREATE TABLE IF NOT EXISTS log (
 uid BIGINT UNSIGNED NOT NULL,
 aid TINYINT UNSIGNED NOT NULL,
@@ -43,7 +70,8 @@ PRIMARY KEY (q)
   for sql in sql_li.split(';') {
     if !sql.is_empty() {
       let sql = sql.to_owned() + ";";
-      println!("{}", sql);
+      println!("\n---{}", sql);
+      let sql = remove_comment(sql);
       QE(sql, &[]).await?;
     }
   }
