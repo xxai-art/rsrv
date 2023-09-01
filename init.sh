c48967b547ab88e704f20dc842aa81251c8ba88a
xxai.art <xxai.art@gmail.com>
2023-09-01 10:45:01 +0800

    ♨

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
-
+cd init
 cargo run -p init
--- init/src/main.rs
+++ init/src/main.rs
@@ -1,3 +1,5 @@
+use std::{env, fs};
+
 use anyhow::Error;
 use gt::QE;
 
@@ -18,56 +20,10 @@ fn remove_comment<S: AsRef<str>>(input: S) -> String {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-  let sql_li = r#"
-CREATE TABLE IF NOT EXISTS seen (
-uid BIGINT UNSIGNED NOT NULL,
-cid TINYINT UNSIGNED NOT NULL,
-rid BIGINT UNSIGNED NOT NULL,
-ts TIMESTAMP(3) NOT NULL,
-TIME INDEX (ts),
-PRIMARY KEY (uid, cid, rid)
-);
+  let sql_li = fs::read_to_string("init.sql")?;
 
-CREATE TABLE IF NOT EXISTS log_n (
-aid TINYINT UNSIGNED NOT NULL,
-cid TINYINT UNSIGNED NOT NULL,
-rid BIGINT UNSIGNED NOT NULL,
-n BIGINT UNSIGNED NOT NULL,
-ts TIMESTAMP(0) NOT NULL,
-TIME INDEX (ts),
-PRIMARY KEY (aid, cid, rid)
-);
-
-# 推荐的点击链
-CREATE TABLE IF NOT EXISTS rec_chain (
-uid BIGINT UNSIGNED NOT NULL,
-aid TINYINT UNSIGNED NOT NULL,
-cid TINYINT UNSIGNED NOT NULL,
-rid BIGINT UNSIGNED NOT NULL,
-rel BIGINT UNSIGNED NOT NULL, # 推荐来源 relationship
-ts TIMESTAMP(3) NOT NULL,
-TIME INDEX (ts),
-PRIMARY KEY (uid, aid, cid, rid, rel)
-);
-
-CREATE TABLE IF NOT EXISTS log (
-uid BIGINT UNSIGNED NOT NULL,
-aid TINYINT UNSIGNED NOT NULL,
-cid TINYINT UNSIGNED NOT NULL,
-rid BIGINT UNSIGNED NOT NULL,
-q BIGINT UNSIGNED NOT NULL,
-ts TIMESTAMP(3) NOT NULL,
-TIME INDEX (ts),
-PRIMARY KEY (uid, aid, cid, rid, q)
-);
-
-CREATE TABLE IF NOT EXISTS q (
-id TIMESTAMP(0) NOT NULL,
-q STRING NOT NULL,
-TIME INDEX (id),
-PRIMARY KEY (q)
-);"#;
   for sql in sql_li.split(';') {
+    let sql = sql.trim_end();
     if !sql.is_empty() {
       let sql = sql.to_owned() + ";";
       println!("\n---{}", sql);
