28cae5fc09a3d9f82d1999d5de3c1d8b29be8c10
xxai.art <xxai.art@gmail.com>
2023-09-09 19:53:27 +0800

    ♨

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -7,12 +7,7 @@ use axum::{
   response::Response,
   Extension,
 };
-
-#[derive(Debug, Clone, Copy)]
-pub struct _Client {
-  pub id: u64,
-  _uid: Option<u64>,
-}
+pub use user::_Client;
 
 pub type Client = Extension<_Client>;
 
@@ -30,58 +25,16 @@ pub async fn client<B>(req: Request<B>, next: Next<B>) -> Result<Response, Statu
 pub async fn _client<B>(mut req: Request<B>, next: Next<B>) -> Result<Response, StatusCode> {
   let cookie = header_get(&req, http::header::COOKIE);
   let host = header_get(&req, http::header::HOST).unwrap();
-  let (has_user, client_id, cookie) = xuser::has_user_client_id_cookie(cookie, host).await;
+  let (client_user, cookie) = xuser::has_user_client_id_cookie(cookie, host).await;
+  req.extensions_mut().insert(_Client(client_user));
 
   Ok(match cookie {
     Some(cookie) => {
-      req.extensions_mut().insert(_Client {
-        id: client_id,
-        _uid: if has_user { Some(0) } else { None },
-      });
       let mut r = next.run(req).await;
       r.headers_mut()
         .insert(http::header::SET_COOKIE, cookie.parse().unwrap());
-
       r
     }
-    None => {
-      req.extensions_mut().insert(_Client {
-        id: client_id,
-        _uid: Some(0),
-      });
-      next.run(req).await
-    }
+    None => next.run(req).await,
   })
-  // let mut client = 0;
-  //
-  // match xuser::client_by_cookie(cookie) {
-  //   ClientState::Renew(id) => {
-  //     client = id;
-  //   }
-  //   ClientState::Ok(id) => {
-  //     req.extensions_mut().insert(_Client { id, _uid: Some(0) });
-  //     return Ok(next.run(req).await);
-  //   }
-  //   _ => {}
-  // }
-  //
-  // let _uid = if client == 0 {
-  //   client = gid!(client);
-  //   Some(0)
-  // } else {
-  //   None
-  // };
-  // req.extensions_mut().insert(_Client { id: client, _uid });
-  // let mut r = next.run(req).await;
-  //
-  // let t = &vbyte::compress_list(&[day10(), client])[..];
-  // let cookie =
-  //   xxai::cookie_encode([&xxh3_64(&[unsafe { &SK }, t].concat()).to_le_bytes()[..], t].concat());
-  // let host = xxai::tld(host);
-  // r.headers_mut().insert(
-  //   http::header::SET_COOKIE,
-  //   format!("I={cookie};max-age=99999999;domain={host};path=/;HttpOnly;SameSite=None;Secure")
-  //     .parse()
-  //     .unwrap(),
-  // );
 }
--- client/src/user.rs
+++ client/src/user.rs
@@ -1,15 +1,13 @@
+use std::ops::{Deref, DerefMut};
+
 use awp::Error;
 use axum::{http::StatusCode, response::IntoResponse};
-use x0::{
-  fred::interfaces::{FunctionInterface, SortedSetsInterface},
-  R,
-};
-use xxai::{ordered_bin_u64, u64_bin_ordered};
+use xuser::ClientUser;
 
-const R_CLIENT_USER: &[u8] = &[4, 0];
-const ZMAX: &str = "zmax";
+#[derive(Debug, Clone, Copy)]
+pub struct _Client(pub ClientUser);
 
-impl crate::_Client {
+impl _Client {
   pub async fn logined(&mut self) -> std::result::Result<u64, awp::Err> {
     if let Some(id) = self.uid().await? {
       return Ok(id);
@@ -18,27 +16,17 @@ impl crate::_Client {
       (StatusCode::UNAUTHORIZED, "need login".to_string()).into_response(),
     )))
   }
+}
 
-  pub async fn is_login(&self, uid: u64) -> anyhow::Result<bool> {
-    let key = &[R_CLIENT_USER, &xxai::u64_bin_ordered(self.id)].concat()[..];
-    let r: Option<u64> = R.zscore(key, &u64_bin_ordered(uid)[..]).await?;
-    Ok(if let Some(s) = r { s > 0 } else { false })
+impl Deref for _Client {
+  type Target = ClientUser;
+  fn deref(&self) -> &<Self as Deref>::Target {
+    &self.0
   }
+}
 
-  pub async fn uid(&mut self) -> anyhow::Result<Option<u64>> {
-    Ok(if let Some(id) = self._uid {
-      if id == 0 {
-        let key = [R_CLIENT_USER, &xxai::u64_bin_ordered(self.id)].concat();
-        // let id: Option<u64> = R.fcall_ro(ZMAX, vec![&key[..]], vec![0]).await?;
-        let id: Option<Vec<u8>> = R.fcall_ro(ZMAX, vec![&key[..]], vec![0]).await.unwrap();
-        let id = id.map(ordered_bin_u64);
-        self._uid = id;
-        id
-      } else {
-        Some(id)
-      }
-    } else {
-      None
-    })
+impl DerefMut for _Client {
+  fn deref_mut(&mut self) -> &mut <Self as Deref>::Target {
+    &mut self.0
   }
 }
