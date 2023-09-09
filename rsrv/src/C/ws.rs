7a7d520143a88e85447060bd5226ddd1d69c677c
xxai.art <xxai.art@gmail.com>
2023-09-09 22:13:28 +0800

    ♨

--- rsrv/src/C/ws.rs
+++ rsrv/src/C/ws.rs
@@ -3,13 +3,12 @@ use int_enum::IntEnum;
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, IntEnum)]
 pub enum WS {
-    未登录 = 0,
-    收藏 = 1,
-    浏览 = 2,
+  收藏 = 0,
+  浏览 = 1,
 }
 
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, IntEnum)]
 pub enum WR {
-    同步 = 0,
+  同步 = 0,
 }
