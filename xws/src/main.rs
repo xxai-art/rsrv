74936bcb16d7360aac39c59c7aa7a07359b8768f
xxai.art <xxai.art@gmail.com>
2023-09-09 13:50:26 +0800

    ♨

--- xws/src/main.rs
+++ xws/src/main.rs
@@ -1,60 +1,86 @@
 use std::net::SocketAddr;
 
-use axum::{
-  extract::{
-    ws::{Message, WebSocket},
-    WebSocketUpgrade,
-  },
-  response::IntoResponse,
-  routing::get,
-  Router, TypedHeader,
+use anyhow::Result;
+use bytes::BytesMut;
+use ratchet_rs::{
+  deflate::DeflateExtProvider, Error, Message, PayloadType, ProtocolRegistry, UpgradedServer,
+  WebSocketConfig,
 };
-use tower_http::trace::{DefaultMakeSpan, TraceLayer};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_stream::{wrappers::TcpListenerStream, StreamExt};
+use tracing::info;
 
-async fn ws_handler(
-  ws: WebSocketUpgrade,
-  user_agent: Option<TypedHeader<headers::UserAgent>>,
-) -> impl IntoResponse {
-  if let Some(TypedHeader(user_agent)) = user_agent {
-    println!("`{}` connected", user_agent.as_str());
+async fn accpet(socket: TcpStream) -> Result<()> {
+  let upgrader = ratchet_rs::accept_with(
+    socket,
+    WebSocketConfig::default(),
+    DeflateExtProvider::default(),
+    ProtocolRegistry::default(),
+  )
+  .await?;
+
+  // You could opt to reject the connection
+  // websocket.reject(WebSocketResponse::new(404)?).await?;
+
+  // Or you could opt to reject the connection with headers
+  // websocket.reject(WebSocketResponse::with_headers(404, headers)?).await;
+
+  let req = &upgrader.request();
+  let uri = {
+    let uri = req.uri().to_string();
+    if let Some(p) = uri.rfind('/') {
+      uri[p + 1..].to_string()
+    } else {
+      return Ok(());
+    }
+  };
+
+  if let Some(cookie) = req.headers().get("cookie") {
+    dbg!(cookie);
   }
 
-  ws.on_upgrade(handle_socket)
-}
+  dbg!(uri);
 
-async fn handle_socket(mut socket: WebSocket) {
-  if let Some(msg) = socket.recv().await {
-    if let Ok(msg) = msg {
-      println!("Client says: {:?}", msg);
-      //客户端发什么，服务端就回什么（只是演示而已）
-      // if socket
-      //   .send(Message::Text(format!("{:?}", msg)))
-      //   .await
-      //   .is_err()
-      // {
-      //   println!("client disconnected");
-      //   return;
-      // }
-    } else {
-      println!("client disconnected");
-      return;
+  let UpgradedServer { websocket, .. } = upgrader.upgrade().await?;
+  let mut buf = BytesMut::new();
+
+  let (mut sender, mut receiver) = websocket.split()?;
+
+  loop {
+    match receiver.read(&mut buf).await? {
+      Message::Text => {
+        dbg!("txt", &buf);
+        sender.write(&mut buf, PayloadType::Text).await?;
+        buf.clear();
+      }
+      Message::Binary => {
+        dbg!("bin", &buf);
+        sender.write(&mut buf, PayloadType::Binary).await?;
+        buf.clear();
+      }
+      Message::Ping(_) | Message::Pong(_) => {
+        dbg!("ping pong");
+      }
+      Message::Close(_) => break,
     }
   }
+  Ok(())
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
   loginit::init();
-  let app = Router::new().route("/-:uid", get(ws_handler)).layer(
-    TraceLayer::new_for_http().make_span_with(DefaultMakeSpan::default().include_headers(true)),
-  );
-
   let addr = SocketAddr::from(([0, 0, 0, 0], envport::get("PORT", 8132)));
+  info!("ws://{}", addr);
 
-  tracing::info!("ws://{}", addr);
+  let listener = TcpListener::bind(addr).await?;
+  let mut incoming = TcpListenerStream::new(listener);
 
-  axum::Server::bind(&addr)
-    .serve(app.into_make_service())
-    .await
-    .unwrap();
+  while let Some(Ok(socket)) = incoming.next().await {
+    trt::spawn!({
+      accpet(socket).await?;
+    });
+  }
+
+  Ok(())
 }
