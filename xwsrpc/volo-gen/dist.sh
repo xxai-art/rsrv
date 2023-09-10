1f18d0cbd68538de632b7db24f76a0e9ae6df068
xxai.art <xxai.art@gmail.com>
2023-09-10 16:20:31 +0800

    ♨

--- xwsrpc/volo-gen/dist.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=$(realpath $0) && DIR=${DIR%/*}
-cd $DIR
-set -ex
-
-cargo v patch -y
-gitsync
-cargo publish --registry crates-io
--- xwsrpc/volo-gen/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![feature(impl_trait_in_assoc_type)]
-
-mod gen {
-  volo::include_service!("volo_gen.rs");
-}
-
-pub use gen::volo_gen::*;
