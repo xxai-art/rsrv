694a03904bcd65fe521fd00cf3c0281fe232d82a
xxai.art <xxai.art@gmail.com>
2023-09-11 14:42:44 +0800

    ♨

--- xws/src/main.rs
+++ xws/src/main.rs
@@ -1,5 +1,6 @@
 #[allow(non_snake_case)]
 mod C;
+mod db;
 mod header_user;
 mod recv;
 use std::net::SocketAddr;
--- xws/src/recv/sync.rs
+++ xws/src/recv/sync.rs
@@ -4,78 +4,8 @@ use tokio::{
   sync::mpsc::{channel, Sender},
   time::{timeout, Duration},
 };
-use xg::Q;
 
 use crate::{AllWs, C::SEND};
-
-const LIMIT: usize = 4096;
-
-Q! {
-    fav_li:SELECT id,cid,rid,ts,aid FROM fav.user WHERE uid=$1 AND id>$2 ORDER BY id LIMIT 4096;
-}
-
-pub async fn 收藏(uid: u64, client_id: u64, mut pre_id: u64, all_ws: AllWs) -> Result<()> {
-  while let Ok(li) = fav_li(uid, pre_id).await {
-    let len = li.len();
-    if len == 0 {
-      break;
-    }
-    let mut r = VecAny::with_capacity(len * 4 + 1);
-    let id = li[len - 1].0;
-    for (_, cid, rid, ts, aid) in li {
-      r.push(cid);
-      r.push(rid);
-      r.push(ts);
-      r.push(aid);
-    }
-    r.push(pre_id);
-    r.push(id);
-    all_ws
-      .to_client(uid, client_id, SEND::收藏, &r.pack())
-      .await?;
-    pre_id = id;
-    if len < LIMIT {
-      break;
-    }
-  }
-  Ok(())
-}
-
-async fn seen_li(uid: u64, ts: u64) -> Result<Vec<(u64, i8, i64)>> {
-  let sql = format!("SELECT CAST(ts as BIGINT) t,cid,rid FROM seen WHERE uid={uid} AND ts>ARROW_CAST({ts},'Timestamp(Millisecond,None)') ORDER BY ts LIMIT 4096");
-  Ok(
-    gt::Q(sql, &[])
-      .await?
-      .into_iter()
-      .map(|i| (i.get::<_, i64>(0) as u64, i.get(1), i.get(2)))
-      .collect(),
-  )
-}
-
-pub async fn 浏览(uid: u64, client_id: u64, mut pre_id: u64, all_ws: AllWs) -> Result<()> {
-  while let Ok(li) = seen_li(uid, pre_id).await {
-    let len = li.len();
-    if len == 0 {
-      break;
-    }
-    let mut r = VecAny::with_capacity(len * 3 + 1);
-    let last_ts = li[len - 1].0;
-    for (ts, cid, rid) in li {
-      r.push(ts);
-      r.push(cid);
-      r.push(rid);
-    }
-    r.push(pre_id);
-    all_ws
-      .to_client(uid, client_id, SEND::浏览, &r.pack())
-      .await?;
-    pre_id = last_ts;
-    if len < LIMIT {
-      break;
-    }
-  }
-  Ok(())
-}
 
 pub async fn sync(msg: &[u8], uid: u64, client_id: u64, all_ws: AllWs) -> Result<()> {
   let body = vb::d(msg)?;
@@ -95,13 +25,13 @@ pub async fn sync(msg: &[u8], uid: u64, client_id: u64, all_ws: AllWs) -> Result
   let sxc = sx.clone();
 
   trt::spawn!({
-    xerr::log!(收藏(uid, client_id, to_sync[0], all).await);
+    xerr::log!(crate::db::fav::sync(uid, client_id, to_sync[0], all).await);
     sxc.send(()).await?;
   });
 
   let all = all_ws.clone();
   trt::spawn!({
-    xerr::log!(浏览(uid, client_id, to_sync[1], all).await);
+    xerr::log!(crate::db::seen::sync(uid, client_id, to_sync[1], all).await);
     sx.send(()).await?;
   });
 
