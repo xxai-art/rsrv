b4338861012cce5e450bf87209883c67d4789a69
xxai.art <xxai.art@gmail.com>
2023-09-11 14:12:04 +0800

    ♨

--- xws/src/recv/mod.rs
+++ xws/src/recv/mod.rs
@@ -5,6 +5,7 @@ use anyhow::Result;
 use crate::{r#type::AllWs, C::RECV};
 
 pub async fn recv(action: RECV, msg: &[u8], uid: u64, client_id: u64, all_ws: AllWs) -> Result<()> {
+  dbg!(&action);
   match action {
     RECV::服务器传浏览器 => {
       sync::sync(msg, uid, client_id, all_ws).await?;
