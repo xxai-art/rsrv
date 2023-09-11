20e491045f2974ed8e7c1122deeaaa0c9ceb4edc
xxai.art <xxai.art@gmail.com>
2023-09-11 15:20:26 +0800

    .

--- xws/src/recv/sync.rs
+++ xws/src/recv/sync.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use anypack::{Pack, VecAny};
+use anypack::{Pack, Packable, VecAny};
 use tokio::{
   sync::mpsc::{channel, Sender},
   time::{timeout, Duration},
@@ -19,32 +19,32 @@ pub async fn sync(msg: &[u8], uid: u64, client_id: u64, all_ws: AllWs) -> Result
       to_sync[p] = i[1];
     }
   }
-  let (sx, mut rx) = channel::<()>(2);
 
-  let all = all_ws.clone();
-  let sxc = sx.clone();
-
+  let (send, mut recv) = channel(3);
   trt::spawn!({
-    xerr::log!(crate::db::fav::sync(uid, client_id, to_sync[0], all).await);
-    sxc.send(()).await?;
+    xerr::log!(crate::db::fav::sync(send, uid, to_sync[1]).await);
   });
 
-  let all = all_ws.clone();
-  trt::spawn!({
-    xerr::log!(crate::db::seen::sync(uid, client_id, to_sync[1], all).await);
-    sx.send(()).await?;
-  });
-
-  let mut n = 0;
-  loop {
-    let _ = timeout(Duration::from_secs(3), rx.recv()).await;
-    n += 1;
-    if n == to_sync.len() {
-      break;
-    }
+  while let Some((action, bin)) = recv.recv().await {
+    all_ws.to_client(uid, client_id, action, &bin).await?;
   }
-  all_ws
-    .to_client(uid, client_id, SEND::服务器传浏览器完成, &[])
-    .await?;
+
+  // let all = all_ws.clone();
+  // trt::spawn!({
+  //   xerr::log!(crate::db::seen::sync(uid, client_id, to_sync[1], all).await);
+  //   sx.send(()).await?;
+  // });
+
+  // let mut n = 0;
+  // loop {
+  //   let _ = timeout(Duration::from_secs(3), rx.recv()).await;
+  //   n += 1;
+  //   if n == to_sync.len() {
+  //     break;
+  //   }
+  // }
+  // all_ws
+  //   .to_client(uid, client_id, SEND::服务器传浏览器完成, &[])
+  //   .await?;
   Ok(())
 }
