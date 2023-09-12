a9e356ad2fa8c303fb4ec956519fb28280fa62f2
xxai.art <xxai.art@gmail.com>
2023-09-12 13:03:55 +0800

    ♨

--- xws/src/recv/log.rs
+++ xws/src/recv/log.rs
@@ -1,8 +1,14 @@
 use anyhow::Result;
+use serde::Deserialize;
 
 use crate::r#type::AllWs;
 
+#[derive(Debug, Deserialize, PartialEq)]
+struct Log(Vec<Vec<Vec<u8>>>);
+
 pub async fn log(level: u8, msg: &[u8], all_ws: AllWs) -> Result<()> {
   dbg!(level, &msg);
+  let log: Log = rmp_serde::from_slice(&msg)?;
+  dbg!(&log);
   Ok(())
 }
