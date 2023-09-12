c90cd8a25c7fe1eb3a5c981dab9a41cdb07ae368
xxai.art <xxai.art@gmail.com>
2023-09-12 13:32:10 +0800

    ♨

--- xws/src/recv/log.rs
+++ xws/src/recv/log.rs
@@ -1,14 +1,35 @@
 use anyhow::Result;
-use serde::Deserialize;
+use msgpacker::prelude::*;
 
 use crate::r#type::AllWs;
 
-#[derive(Debug, Deserialize, PartialEq)]
-struct Log(Vec<Vec<Vec<u8>>>);
+#[derive(Debug, PartialEq, Eq, MsgPacker)]
+struct Log {
+  li: Vec<Vec<u8>>,
+}
 
-pub async fn log(level: u8, msg: &[u8], all_ws: AllWs) -> Result<()> {
-  dbg!(level, &msg);
-  let log: Log = rmp_serde::from_slice(&msg)?;
-  dbg!(&log);
+#[derive(Debug, PartialEq, Eq, MsgPacker)]
+struct LogLi {
+  li: Vec<Log>,
+}
+
+pub async fn log(level: u8, buf: &[u8], all_ws: AllWs) -> Result<()> {
+  dbg!(level, &buf);
+  let (_, log_li) = LogLi::unpack(&buf)?;
+
+  for li in log_li.li {
+    let li = li.li;
+    if li.len() > 1 {
+      let q = &li[0];
+      let q = if q.is_empty() {
+        "".into()
+      } else {
+        String::from_utf8_lossy(q)
+      };
+      let li = &li[1..];
+      dbg!(q, li);
+    }
+  }
+
   Ok(())
 }
